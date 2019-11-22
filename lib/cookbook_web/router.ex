c6caedcfc847d806a8103a2f8e2348f9dfa26e95
Cohen Carlisle <cohencarlisle+github@pm.me>
2019-11-21 21:42:03 -0500

    mix phx.new cookbook

---
+++ lib/cookbook_web/router.ex
@@ -0,0 +1,26 @@
+defmodule CookbookWeb.Router do
+  use CookbookWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", CookbookWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", CookbookWeb do
+  #   pipe_through :api
+  # end
+end
